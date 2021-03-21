d458131692622f7fd37809170deb2371eabf8324
jimender2 <35303639+jimender2@users.noreply.github.com>
2021-03-21 10:59:20 -0400

    Add new line to boot_sect_hello.

--- boot_sect_hello.asm
+++ boot_sect_hello.asm
@@ -1,29 +1,34 @@
-mov ah, 0x0e ; tty mode
-mov al, 'H'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-int 0x10 ; 'l' is still on al, remember?
-mov al, 'o'
-int 0x10
-mov al, ' '
-int 0x10
-mov al, 'W'
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, 'r'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'd'
-int 0x10
-mov al, '!'
-int 0x10
+loop:
+    mov ah, 0x0e ; tty mode
+    mov al, 'H'
+    int 0x10
+    mov al, 'e'
+    int 0x10
+    mov al, 'l'
+    int 0x10
+    int 0x10 ; 'l' is still on al, remember?
+    mov al, 'o'
+    int 0x10
+    mov al, ' '
+    int 0x10
+    mov al, 'W'
+    int 0x10
+    mov al, 'o'
+    int 0x10
+    mov al, 'r'
+    int 0x10
+    mov al, 'l'
+    int 0x10
+    mov al, 'd'
+    int 0x10
+    mov al, '!'
+    int 0x10
+    mov al, 0xa
+    int 0x10
+    mov al, 0xd
+    int 0x10
 
-jmp $ ; jump to current address = infinite loop
+jmp loop ; jump to current address = infinite loop
 
 ; padding and magic number
 times 510 - ($-$$) db 0
